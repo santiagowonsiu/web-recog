3b367b28338fe654ff90b56759d3037cf3f4a225
santiagowonsiu <147046858+santiagowonsiu@users.noreply.github.com>
2024-02-23 13:43:28 +0000

    Working Mediapipe drawings in the website

--- views.py
+++ views.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, jsonify, redirect, url_fo
 import random
 import numpy as np
 import cv2
+import mediapipe as mp
+import pickle
 
 #### SETUP
 views = Blueprint(__name__, "views")
@@ -43,17 +45,62 @@ def generate_objects(num_objects=1):
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
+
+with open('body_language.pkl', 'rb') as f:
+    model = pickle.load(f)
+
 def gen_frames():
     camera = cv2.VideoCapture(0)  # Use 0 for web camera
 
-    while True:
-        success, frame = camera.read()  # Read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while True:
+            success, frame = camera.read()  # Read the camera frame
+            if not success:
+                break
+            else:
+                # Recolor Feed
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False        
+
+                # Make Detections
+                results = holistic.process(image)
+
+                # Recolor image back to BGR for rendering
+                image.flags.writeable = True   
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+                try:
+                    # Draw face landmarks
+                    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
+                                            mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+                                            mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                                            )
+
+                    # Right hand
+                    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                            mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                                            mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                                            )
+
+                    # Left Hand
+                    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                            mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                                            mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                                            )
+
+                    # Pose Detections
+                    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
+                                            mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                            mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                            )
+                except:
+                    pass
+
+                ret, buffer = cv2.imencode('.jpg', image)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
     camera.release()
